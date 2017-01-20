f615989f2da53da4b317d87463fbc4992c4eb4f0
sergiimarchuk <sergii.marchuk@gmail.com>
2017-01-20 21:57:49 +0200

    Update job_cron_collect.sh

--- job_cron_collect.sh
+++ job_cron_collect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/e220314/qa_py/; ansible-playbook -i inventory qalpy.yml; ./pars_log.py
+cd /home/USERNAMEHOMEFOLDER/qa_py/; ansible-playbook -i inventory qalpy.yml; ./pars_log.py
